3bfbb745cc2aa6a2b1045f67fd0a39eb3a53499b
PascualFort <fortpascual@gmail.com>
2023-03-05 17:45:39 +0100

    Slugify nodemaon console message

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-const express = require('express')
-const products_routes = require('./routes/products.js')
+const express = require("express");
+const products_routes = require("./routes/products.js");
+const slugify = require("slugify");
 
 //Server instantiation
-const app = express()
+const app = express();
 
 //Server configuration: template engine
-app.set('views', './views');
-app.set('view engine', 'pug');
-app.use(express.static('/views'));
+app.set("views", "./views");
+app.set("view engine", "pug");
+app.use(express.static("/views"));
 
 //Midleware
-app.use(express.json())
-app.use('/', products_routes)
+app.use(express.json());
+app.use("/", products_routes);
 
 //Server startup
 app.listen(5000, () => {
-    console.log('server is listening on port 5000')
-})
-
-
+  console.log(slugify("server is listening on port 5000", "*"));
+});
